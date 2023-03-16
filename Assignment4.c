d0936cfd3a9b440f52e29543042b0a352b9b2d3a
Dhruv071 <127030697+Dhruv071@users.noreply.github.com>
2023-03-16 23:45:47 +0530

    Update Assignment4.c

--- Assignment4.c
+++ Assignment4.c
@@ -11,7 +11,10 @@ for(i=0;i<3;i++)
     for(j=0;j<3;j++)
   {
       printf("\t%d ",matrix[i][j]);
-	       if(max<matrix[i][j]){max=matrix[i][j];}
+	       if(max<matrix[i][j])
+	       {
+		       max=matrix[i][j];
+	       }
   }
   printf("\n");
 }
